d112c35fef854d22dd32e4c0a7b59b2239ac1415
simon despres <simondprs62790@hotmail.fr>
2023-10-21 10:19:56 +0200

    integrate strapi with next.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,43 @@
  */
 import Card from "../components/card/card";
 import { IconTypes } from "@/components/button/button"
+import config from "@/config";
 
-const Home = () => (
+const fetchblogs = async (params) => {
+  const init = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`
+    }
+  }
+  const request = await fetch(`${config.api}/api/blogs?populate=*&${params}`, init);
+  const response = await request.json();
+
+  return response;
+}
+
+const Home = async () => {
+  const [featuredBlogs, blogs] = await Promise.all([
+    await fetchblogs('filters[IsFeatured][$eq]=true'),
+    await fetchblogs('filters[IsFeatured][$eq]=false')
+  ])
+  console.log(featuredBlogs.data);
+  return (
     <div className="container pb-80">
-      <Card
-            label="Product Reviews"
-            title="Class aptent taciti socio squad litoral torquent per conubia nostra"
-            summary="Mauris Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, enim."
-            href="#"
-            btnIcon={ IconTypes.ARROW_RIGHT }
-            // btnLabel={}
-            className="mb-30"
-          />
+      {featuredBlogs.data.map(featuredBlog => (
+        <Card
+          key={featuredBlog.id}
+          label={featuredBlog.attributes.Category}
+          title={featuredBlog.attributes.Title}
+          summary={featuredBlog.attributes.Summary}
+          imgSrc={`${config.api}${featuredBlog.attributes.FeaturedImage.data.attributes.url}`}
+          imgAlt="Featured Image"
+          href={`/${featuredBlog.attributes.slug}`}
+          btnIcon={ IconTypes.ARROW_RIGHT }
+          // btnLabel={}
+          className="mb-30"
+        />
+      ))}
       <div className="row">
         <div className="col col_4 m-mw-100">
           <Card
@@ -52,5 +77,5 @@ const Home = () => (
       </div>
     </div>
   )
-
+};
 export default Home;
