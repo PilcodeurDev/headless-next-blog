47119063b358072af74b16a09d8b65249d6acf3d
simon despres <simondprs62790@hotmail.fr>
2023-10-23 23:40:28 +0200

    take next: 13.4.19

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // // output: 'export',
-  // // distDir: 'dist',
-  // images: {
-  //   formats: ['image/avif', 'image/webp'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: '127.0.0.1',
-  //       // port: '',
-  //       pathname: '/uploads/**',
-  //     },
-  //   ],
-  // },
+  // output: 'export',
+  distDir: 'dist',
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        // port: '',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
